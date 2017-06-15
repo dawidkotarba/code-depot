da124aa306bfbc25e2d14814de73ef8661a0bb5c
dawidkotarba <dawidkotarba>
2017-06-16 01:46:21 +0200

    update tests

--- src/test/groovy/app/auth/service/impl/AuthenticationServiceImplSpec.groovy
+++ src/test/groovy/app/auth/service/impl/AuthenticationServiceImplSpec.groovy
@@ -11,14 +11,34 @@ class AuthenticationServiceImplSpec extends Specification {
     def underTest = new AuthenticationServiceImpl(daoAuthenticationProvider)
 
     @Unroll
-    def "should authenticate user"() {
+    def "should authenticate user as #isAuthenticated"() {
         given:
         def authenticationResult = Stub(Authentication)
-        authenticationResult.authenticated >> true
+        authenticationResult.authenticated >> isAuthenticated
         daoAuthenticationProvider.authenticate(_ as Authentication) >> authenticationResult
 
         expect:
         Authentication result = underTest.authenticate(_ as String, _ as String)
-        result.authenticated == true
+        result.authenticated == expectedResult
+
+        where:
+        isAuthenticated | expectedResult
+        false           | false
+        true            | true
+    }
+
+    @Unroll
+    def "should not allow putting empty data for username: #username and password: #password"() {
+        when:
+        underTest.authenticate(username, password)
+
+        then:
+        thrown Exception
+
+        where:
+        username | password
+        null     | null
+        _        | null
+        null     | _
     }
 }
