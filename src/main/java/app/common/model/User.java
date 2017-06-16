5eb25218c121098e82c50ff0c824c77f79045a28
dawidkotarba <dawidkotarba>
2017-06-16 11:33:14 +0200

    remove meeting document and repo

--- src/main/java/app/common/model/User.java
+++ src/main/java/app/common/model/User.java
@@ -2,21 +2,14 @@ package app.common.model;
 
 import app.auth.model.UserRole;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Set;
 
-@Document
 @Data
-public class User implements UserDetails {
-
-    @Id
-    private String id;
+public class User extends AbstractDocument implements UserDetails {
 
     @Indexed(unique = true)
     private String username;
@@ -29,5 +22,4 @@ public class User implements UserDetails {
     private boolean credentialsNonExpired;
     private Date creationDate;
     private Set<UserRole> authorities;
-    private Set<Meeting> meetings = new HashSet<>();
 }
